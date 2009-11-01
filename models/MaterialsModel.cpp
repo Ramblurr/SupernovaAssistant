8c3de6bb6fe9fd81d3cb23298c8df05f292e27be
Casey Link <unnamedrambler@gmail.com>
2009-11-01 12:19:36 -0500

    debug--

--- models/MaterialsModel.cpp
+++ models/MaterialsModel.cpp
@@ -19,6 +19,7 @@ Qt::ItemFlags MaterialsModel::flags( const QModelIndex &index ) const
 
 void MaterialsModel::item_changed( const SNItem &item, quint64 diff )
 {
+//    qDebug() << item.name() << "changed";
     if( diff == 0 )
         return;
     QMap<QString, int> comps = item.getComponents();
