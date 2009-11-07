96cc288ce614d87555886c6e4b640096cfcb2ba8
Casey Link <unnamedrambler@gmail.com>
2009-11-07 16:36:51 -0500

    CMake: add a test to see if the installed sqlite version supports FKs

--- cmake/modules/FindSqlite.cmake
+++ cmake/modules/FindSqlite.cmake
@@ -5,16 +5,18 @@
 #  SQLITE_INCLUDE_DIR - the Sqlite include directory
 #  SQLITE_LIBRARIES - Link these to use Sqlite
 #  SQLITE_DEFINITIONS - Compiler switches required for using Sqlite
+#  SQLITE_FK_SUPPORT - sqlite is >=version 3.6.19, and hence has Foreign Key Support
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 #
 
 
 # Copyright (c) 2008, Gilles Caulier, <caulier.gilles@gmail.com>
+# Copyright (c) 2009, Casey Link, <unnamedrambler@gmail.com>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
-
+include(CheckCXXSourceRuns)
 if ( SQLITE_INCLUDE_DIR AND SQLITE_LIBRARIES )
    # in cache already
    SET(Sqlite_FIND_QUIETLY TRUE)
@@ -25,7 +27,7 @@ endif ( SQLITE_INCLUDE_DIR AND SQLITE_LIBRARIES )
 if( NOT WIN32 )
   find_package(PkgConfig)
 
-  pkg_check_modules(PC_SQLITE QUIET sqlite3)
+  pkg_check_modules(PC_SQLITE sqlite3)
 
   set(SQLITE_DEFINITIONS ${PC_SQLITE_CFLAGS_OTHER})
 endif( NOT WIN32 )
@@ -41,6 +43,27 @@ FIND_LIBRARY(SQLITE_LIBRARIES NAMES sqlite3
   ${PC_SQLITE_LIBDIR}
   ${PC_SQLITE_LIBRARY_DIRS}
 )
+set( CMAKE_REQUIRED_LIBRARIES ${SQLITE_LIBRARIES})
+set( CMAKE_REQUIRED_INCLUDES ${SQLITE_INCLUDE_DIR})
+set( CMAKE_REQUIRED_DEFINITIONS ${SQLITE_DEFINITIONS})
+set(CMAKE_REQUIRED_FLAGS "")
+CHECK_CXX_SOURCE_RUNS("
+#include <sqlite3.h>
+ int main()
+ {
+ #ifndef SQLITE_VERSION
+ #error \"SQLite header version number not found\"
+ #endif
+ #if SQLITE_VERSION_NUMBER < 3006019
+ #error \"SQLite header version 3006019 not found.\"
+ #endif
+
+ //Check if library version matches header version.
+ int libver = sqlite3_libversion_number();
+ if( libver != SQLITE_VERSION_NUMBER )
+    return 1;
+ return 0;
+}" SQLITE_FK_SUPPORT)
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(Sqlite DEFAULT_MSG SQLITE_INCLUDE_DIR SQLITE_LIBRARIES )
