ce8af08ad2a251b6018ea31fd14c59552a6db367
Casey Link <unnamedrambler@gmail.com>
2009-12-12 08:54:35 -0500

    Tests: add parsePMAP test

--- tests/TurnParserTest.cpp
+++ tests/TurnParserTest.cpp
@@ -35,16 +35,16 @@ void TurnParserTest::sss()
 
     System sys = parser->parseSS(data);
 
-    QVERIFY( sys.name() == "Test1" );
+    QVERIFY( sys.name() == "TEST1" );
     QVERIFY( sys.starType() == "S (Dim Red) 1 IV" );
     QVERIFY( sys.starSize() == "Single Star" );
 
 
     QVERIFY( sys.warpPointsCount() == 3);
 
-    WarpPoint wp1(1234, "Test1", "C", 1594.1);
-    WarpPoint wp2(12345, "Test1", "A", 2143.9);
-    WarpPoint wp3(4321, "Test1", "C", 679.8);
+    WarpPoint wp1(1234, "TEST1", "C", 1594.1);
+    WarpPoint wp2(12345, "TEST1", "A", 2143.9);
+    WarpPoint wp3(4321, "TEST1", "C", 679.8);
 
     QVERIFY( sys.warpPoints().contains(wp1) );
     QVERIFY( sys.warpPoints().contains(wp2) );
@@ -52,14 +52,14 @@ void TurnParserTest::sss()
 
     QVERIFY( sys.orbitsCount() == 8 );
 
-    Planet p1("Test1-1","Test1", "1", "", "Hot Terrestrial", 0.4, 44733, "Chlorine", "");
-    Planet p2("Test1-2","Test1", "2", "", "Hot Terrestrial", 0.75, 34089, "Chlorine", "");
-    Planet p3("Test1-3","Test1", "3", "", "Asteroid Field", 1.1, 1, "Vacuum", "");
-    Planet p4("Test1-4","Test1", "4", "", "Terrestrial", 1.8, 18468, "Carbon Dioxide", "");
-    Planet p5("Test1-4a","Test1", "4", "a", "Moon", 0.0, 3233, "Nitrogen", "");
-    Planet p6("Test1-4b","Test1", "4", "b", "Moon", 0.0, 2166, "Vacuum", "");
-    Planet p7("Test1-4c","Test1", "4", "c", "Moon", 0.0, 3119, "Chlorine", "");
-    Planet p8("Test1-5","Test1", "5", "", "Frozen Terrestrial", 3.2, 33430, "Vacuum", "");
+    Planet p1("TEST1-1","TEST1", "1", "", "Hot Terrestrial", 0.4, 44733, "Chlorine", "");
+    Planet p2("TEST1-2","TEST1", "2", "", "Hot Terrestrial", 0.75, 34089, "Chlorine", "");
+    Planet p3("TEST1-3","TEST1", "3", "", "Asteroid Field", 1.1, 1, "Vacuum", "");
+    Planet p4("TEST1-4","TEST1", "4", "", "Terrestrial", 1.8, 18468, "Carbon Dioxide", "");
+    Planet p5("TEST1-4a","TEST1", "4", "a", "Moon", 0.0, 3233, "Nitrogen", "");
+    Planet p6("TEST1-4b","TEST1", "4", "b", "Moon", 0.0, 2166, "Vacuum", "");
+    Planet p7("TEST1-4c","TEST1", "4", "c", "Moon", 0.0, 3119, "Chlorine", "");
+    Planet p8("TEST1-5","TEST1", "5", "", "Frozen Terrestrial", 3.2, 33430, "Vacuum", "");
 
     QVERIFY(sys.orbits().contains(p1));
     QVERIFY(sys.orbits().contains(p2));
@@ -80,16 +80,16 @@ void TurnParserTest::sss()
 
     sys = parser->parseSS(data1);
 
-    QVERIFY( sys.name() == "Test2" );
+    QVERIFY( sys.name() == "TEST2" );
     QVERIFY( sys.starType() == "M (Red) 1 V" );
     QVERIFY( sys.starSize() == "Single Star" );
 
 
     QVERIFY( sys.warpPointsCount() == 3);
 
-    WarpPoint test2wp1(12345, "Test2", "D", 1.9);
-    WarpPoint test2wp2(234567, "Test2", "E", 596.7);
-    WarpPoint test2wp3(345678, "Test2", "D", 0.2);
+    WarpPoint test2wp1(12345, "TEST2", "D", 1.9);
+    WarpPoint test2wp2(234567, "TEST2", "E", 596.7);
+    WarpPoint test2wp3(345678, "TEST2", "D", 0.2);
 
     QVERIFY( sys.warpPoints().contains(test2wp1) );
     QVERIFY( sys.warpPoints().contains(test2wp2) );
@@ -97,12 +97,12 @@ void TurnParserTest::sss()
 
     QVERIFY( sys.orbitsCount() == 6 );
 
-    Planet test2p1("Test2-1","Test2", "1", "", "Terrestrial", 0.6, 87840, "Ammonia", "");
-    Planet test2p2("Test2-2","Test2", "2", "", "Frozen Terrestrial", 1, 140120, "Oxygen", "");
-    Planet test2p3("Test2-3","Test2", "3", "", "Gas Giant", 1.4, 0.0, "---", "Ion Storm");
-    Planet test2p4("Test2-3a","Test2", "3", "a", "Moon", 0.0, 9315, "Vacuum", "");
-    Planet test2p5("Test2-3b","Test2", "3", "b", "Moon", 0.0, 4911, "Vacuum", "");
-    Planet test2p6("Test2-4","Test2", "4", "", "Frozen Rockball", 2.2, 85440, "Chlorine", "Ion Storm");
+    Planet test2p1("TEST2-1","TEST2", "1", "", "Terrestrial", 0.6, 87840, "Ammonia", "");
+    Planet test2p2("TEST2-2","TEST2", "2", "", "Frozen Terrestrial", 1, 140120, "Oxygen", "");
+    Planet test2p3("TEST2-3","TEST2", "3", "", "Gas Giant", 1.4, 0.0, "---", "Ion Storm");
+    Planet test2p4("TEST2-3a","TEST2", "3", "a", "Moon", 0.0, 9315, "Vacuum", "");
+    Planet test2p5("TEST2-3b","TEST2", "3", "b", "Moon", 0.0, 4911, "Vacuum", "");
+    Planet test2p6("TEST2-4","TEST2", "4", "", "Frozen Rockball", 2.2, 85440, "Chlorine", "Ion Storm");
 
     QVERIFY(sys.orbits().contains(test2p1));
     QVERIFY(sys.orbits().contains(test2p2));
@@ -111,6 +111,66 @@ void TurnParserTest::sss()
     QVERIFY(sys.orbits().contains(test2p5));
     QVERIFY(sys.orbits().contains(test2p6));
 
+}
+
+void TurnParserTest::pmaps()
+{
+    DEBUG_BLOCK
+
+    QFile f(":/tests/data/PMAP.txt");
+    if (!f.open(QIODevice::ReadOnly))
+        return;
+
+    QByteArray array = f.readAll();
+    f.close();
+    QString data(array);
+
+    Planet planet = parser->parsePMAP(data);
+
+    QVERIFY(planet.name() == "TEST1-2");
+    QVERIFY(planet.temp() == "311");
+    QVERIFY(planet.axialTilt() == "30");
+    QVERIFY(planet.gravity() == "1.1");
+    QVERIFY(planet.atmosphere() == "Chlorine");
+    QVERIFY(planet.ocean() == "Water");
+
+    QVERIFY(planet.terrain().size() == 7);
+
+    QVERIFY(planet.terrain().contains(TerrainPair("Desert", "23")));
+    QVERIFY(planet.terrain().contains(TerrainPair("Forested Mountains", "19")));
+    QVERIFY(planet.terrain().contains(TerrainPair("Rain Forest", "16")));
+    QVERIFY(planet.terrain().contains(TerrainPair("Deciduous Forest", "10")));
+    QVERIFY(planet.terrain().contains(TerrainPair("Marsh", "10")));
+    QVERIFY(planet.terrain().contains(TerrainPair("Salt Flats", "9")));
+
+
+    QFile f2(":/tests/data/PMAP2.txt");
+    if (!f2.open(QIODevice::ReadOnly))
+        return;
+
+    QByteArray array2 = f2.readAll();
+    f2.close();
+    QString data2(array2);
+
+    Planet planet2 = parser->parsePMAP(data2);
+    QVERIFY(planet2.atmosphere() == "Carbon Dioxide");
+    QVERIFY(planet2.ocean() == "Liquid Ammonia");
+    QVERIFY(planet2.terrain().contains(TerrainPair("Liquid Ammonia", "65")));
+
+    QFile f3(":/tests/data/PMAP3.txt");
+    if (!f3.open(QIODevice::ReadOnly))
+        return;
+
+    QByteArray array3 = f3.readAll();
+    f3.close();
+    QString data3(array3);
+
+    Planet planet3 = parser->parsePMAP(data3);
+    QVERIFY(planet3.atmosphere() == "Vacuum");
+    QVERIFY(planet3.ocean() == "-None-");
+    QVERIFY(planet3.axialTilt() == "----");
+    QVERIFY(planet3.gravity() == "0.1");
+    QVERIFY(planet3.terrain().contains(TerrainPair("Asteroids", "100")));
 }
 
 void TurnParserTest::anzs()
--- tests/TurnParserTest.h
+++ tests/TurnParserTest.h
@@ -17,6 +17,7 @@ public:
 private slots:
     void anzs();
     void sss();
+    void pmaps();
 
 private:
     TurnParser* parser;
